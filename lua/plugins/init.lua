7e10342bc62c98e098d6042411317630cc2fc135
Phil Cunliffe <phillip.cunliffe@gmail.com>
2024-09-16 20:40:36 -0700

    Starting prettier addition

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,7 +12,42 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
 
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "garden",
+          path = "~/Documents/garden",
+        },
+      },
+
+      -- see below for full list of options 👇
+    },
+    {
+      "MunifTanjim/prettier.nvim",
+      dependencies = {
+        "jose-elias-alvarez/null-ls.nvim"
+      }
+    }
+  }
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
